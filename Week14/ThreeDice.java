5d58d31c99bc55a33a3f1c8c702f996b53a4c64c
SAC-CS112-Nordwick-Donald <donnordwick@gmail.com>
2015-11-27 14:07:28 -0800

    Update

--- Week14/ThreeDice.java
+++ Week14/ThreeDice.java
@@ -5,7 +5,7 @@ public int Throw(){
 	int total = 0;
 	int average = 0;
 	for (int i =0; i<3;i++){
-	total = total +super.Throw();
+	total = total + super.Throw();
 	}
 	average = total/3;
 	return average;
